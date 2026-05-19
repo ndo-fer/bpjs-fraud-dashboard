b4520cfeb352a20403a794795ef933de9226fcfe
ndo-fer <email-yang-terhubung-ke-akun-ndo-fer@github.com>
2026-05-20 00:53:55 +0700

    initial dashboard setup

---
+++ app.py
@@ -0,0 +1,9 @@
+﻿import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard PA",
+    layout="wide"
+)
+
+st.title("Dashboard Prioritas Verifikasi Klaim")
+st.write("Prototype dashboard PA dari nol.")
